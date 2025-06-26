3bd4ffc573b56bb6f529a2a5581ebad500ccd1d5
Maarten Casteels <denmette@gmail.com>
2025-06-26 08:54:56 +0200

    chore: clean up

--- tests/Unit/EventModelTest.php
+++ tests/Unit/EventModelTest.php
@@ -3,15 +3,8 @@
 use App\Models\Event;
 use App\Models\Link;
 
-it('uses slug as route key name', function () {
-    $event = Event::factory()->create();
+it("uses slug as route key name", function () {
+    $event = Event::factory()->make();
 
-    expect($event->getRouteKeyName())->toBe('slug');
-});
-
-it('has many links', function () {
-    $event = Event::factory()->has(Link::factory()->count(2))->create();
-
-    expect($event->links)->toHaveCount(2);
-    expect($event->links->first())->toBeInstanceOf(Link::class);
+    expect($event->getRouteKeyName())->toBe("slug");
 });
