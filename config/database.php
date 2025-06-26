04a294843e428b1001596b2f8b6e2135d6af8fd6
Maarten Casteels <denmette@gmail.com>
2025-06-26 20:46:21 +0200

    chore: remove unused features and add redirect config

--- config/database.php
+++ config/database.php
@@ -1,8 +1,26 @@
 <?php
 
 return [
-    "migrations" => [
-        "table" => "migrations",
-        "update_date_on_publish" => false, // disable to preserve original behavior for existing applications
+    'default' => 'pgsql',
+
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => '127.0.0.1',
+            'port' => 5432,
+            'database' => 'topvorm_cursus',
+            'username' => 'postgres',
+            'password' => 'postgres',
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+
+    'migrations' => [
+        'table' => 'migrations',
+        'update_date_on_publish' => false,
     ],
 ];
